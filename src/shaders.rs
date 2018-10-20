39906da76704fdbcb40fd087e4172b560fd07ecc
Jan van Brügge <supermanitu@gmail.com>
2018-10-20 22:58:02 +0300

    Move fragment shader to new file

--- src/shaders.rs
+++ src/shaders.rs
@@ -20,28 +20,7 @@ void main() {
 mod fs {
     #[derive(VulkanoShader)]
     #[ty = "fragment"]
-    #[src = "
-#version 450
-
-#define PI 3.1415926535897932384626433832795
-
-layout(location = 0) out vec4 f_color;
-
-layout(push_constant) uniform Data {
-    float timer;
-} data;
-
-
-void main() {
-    const float inner_timer = data.timer;
-    f_color = vec4(
-        sin(inner_timer),
-        sin(inner_timer * PI / 3),
-        sin(inner_timer * PI / 3 * 2),
-        1.0
-    );
-}
-"]
+    #[path = "shaders/fragment.glsl"]
     #[allow(dead_code)]
     struct Dummy;
 }
